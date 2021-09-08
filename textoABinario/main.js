3295fce49dcf8588f7a32dd428e39c9e355ee3c3
adriantafoya <adriantm214@gmail.com>
2021-09-08 10:43:01 -0700

    Terminado el conversor de texto a binario

--- textoABinario/main.js
+++ textoABinario/main.js
@@ -1,92 +1,69 @@
-const resultado = document.querySelector("#txt-entrada");
+"use strict";
+
+const inputTexto = document.querySelector("#txt-entrada"),
+	btnGenerar = document.querySelector('#btn-generar'),
+	contenedorResultado = document.querySelector('#resultado');
 
 
+function generarCodigo () {
+	let texto = inputTexto.value;
+	let arrayTexto = texto.split('');
+	let arrayCodigoASCII = [];
+	let codigoBinario = [];
 
-// BTN.addEventListener('click', () => {
-//   const CARACTER = {
-//     " ": "00100000",
-//     "0": "00110000",
-//     "1": "00110001",
-//     "2": "00110010",
-//     "3": "00110011",
-//     "4": "00110100",
-//     "5": "00110101",
-//     "6": "00110110",
-//     "7": "00110111",
-//     "8": "00111000",
-//     "9": "00111001",
-//     A: "01000001",
-//     "Á": "11000001",
-//     B: "01000010",
-//     C: "01000011",
-//     D: "01000100",
-//     E: "01000101",
-//     "É": "11001001",
-//     F: "01000110",
-//     G: "01000111",
-//     H: "01001000",
-//     I: "01001001",
-//     "Í": "11001101",
-//     J: "01001010",
-//     K: "01001011",
-//     L: "01001100",
-//     M: "01001101",
-//     N: "01001110",
-//     "Ñ": "11010001",
-//     O: "01001111",
-//     "Ó": "11010011",
-//     P: "01010000",
-//     Q: "01010001",
-//     R: "01010010",
-//     S: "01010011",
-//     T: "01010100",
-//     U: "01010101",
-//     "Ú": "11011010",
-//     "Ü": "11011100",
-//     V: "01010110",
-//     W: "01010111",
-//     X: "01011000",
-//     Y: "01011001",
-//     Z: "01011010",
-//     a: "01100001",
-//     "á": "11100001",
-//     b: "01100010",
-//     c: "01100011",
-//     d: "01100100",
-//     e: "01100101",
-//     "é": "11101001",
-//     f: "01100110",
-//     g: "01100111",
-//     h: "01101000",
-//     i: "01101001",
-//     "í": "11101101",
-//     j: "01101010",
-//     k: "01101011",
-//     l: "01101100",
-//     m: "01101101",
-//     n: "01101110",
-//     "ñ": "11110001",
-//     o: "01101111",
-//     "ó": "11110011",
-//     p: "01110000",
-//     q: "01110001",
-//     r: "01110010",
-//     s: "01110011",
-//     t: "01110100",
-//     u: "01110101",
-//     "ú": "11111010",
-//     "ü": "11111100",
-//     v: "01110110",
-//     w: "01110111",
-//     x: "01111000",
-//     y: "01111001",
-//     z: "01111010",
-//   }
-  
-//   let entrada = document.getElementById("entrada").value;
-//   let arrayEntrada = entrada.split('').map((elemento) => (
-//     CARACTER[elemento]
-//   ));
-  
-//   resultado.innerText = arrayEntrada.join(' ');
-// });
+	arrayTexto.forEach((elemento, x) => {
+		arrayCodigoASCII[x] = elemento.charCodeAt();
+	});
+
+	arrayCodigoASCII.forEach((elemento, x) => {
+		codigoBinario.push(conversorBinario(elemento));
+	});
+
+	codigoBinario = codigoBinario.join(" ");
+
+	contenedorResultado.textContent = codigoBinario;
+}
+
+function conversorBinario (ascii) {
+	let codigoBin = [];
+	let x = ascii;
+
+	while (ascii > 0) {
+		codigoBin.unshift(ascii % 2);
+		ascii = parseInt(ascii / 2);
+	}
+
+	codigoBin = verificarBits(codigoBin.join(""));
+
+	return codigoBin;
+}
+
+function verificarBits (bin) {
+	if (bin.length < 8) {
+		let binCorregido;
+
+		switch (bin.length) {
+			case 1: binCorregido = "0000000" + bin; 
+			break;
+			case 2: binCorregido = "000000" + bin; 
+			break;
+			case 3: binCorregido = "00000" + bin; 
+			break;
+			case 4: binCorregido = "0000" + bin; 
+			break;
+			case 5: binCorregido = "000" + bin; 
+			break;
+			case 6: binCorregido = "00" + bin; 
+			break;
+			case 7: binCorregido = "0" + bin; 
+			break;
+		}
+
+		return binCorregido;
+	}
+}
+
+btnGenerar.addEventListener('click', generarCodigo);
+inputTexto.addEventListener('keydown', (e) => {
+	if (e.keyCode === 13) generarCodigo();
+});
